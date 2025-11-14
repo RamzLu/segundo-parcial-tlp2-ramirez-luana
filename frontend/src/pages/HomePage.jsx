13bea6ab07919df3cbb1d0152476c498aeee2334
Ramirez Luana <luanaabigail168@gmail.com>
2025-11-14 16:35:45 -0300

    home semi terminada

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 export const HomePage = () => {
   // TODO: Integrar lógica para obtener superhéroes desde la API
   // TODO: Implementar useState para almacenar la lista de superhéroes
   // TODO: Implementar función para recargar superhéroes
-
   // Datos de ejemplo para las cards
   const superheroes = [
     {
@@ -42,18 +43,70 @@ export const HomePage = () => {
         "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/149-captain-america.jpg",
     },
   ];
+  // const [superheroes, setSuperheroes] = useState([]);
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  // const fetchSuperheroes = async () => {
+  //   try {
+  //     const response = await fetch("http://localhost:3000/api/superheroes", {
+  //       credentials: "include",
+  //     });
 
+  //     if (!response.ok) throw new Error("Error al obtener superhéroes");
+
+  //     const data = await response.json();
+  //     setSuperheroes(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  const fetchUser = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/profile", {
+        credentials: "include",
+      });
+      if (!response.ok) {
+        throw new Error("Error fetch profile");
+      }
+      const data = await response.json();
+      console.log(data);
+      setUser(data.user);
+    } catch (error) {
+      console.log(error);
+      navigate("/login");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+    // fetchSuperheroes();
+  }, [navigate]);
+
+  const userName = user?.name;
+
+  // const [superhero, setSuperhero] = useState([]);
+  // useEffect(() => {
+  //   setSuperhero(superheroes);
+  // }, []);
+
+  // const handleReload = () => {
+  //   setSuperhero([...superhero]);
+  // };
   return (
     <div className="container mx-auto px-4 pb-8">
+      <h2 className="text-4xl font-bold text-center mt-8 mb-4 text-gray-800">
+        Bienvenido/a {userName}!
+      </h2>
       <h1 className="text-4xl font-bold text-center mt-8 mb-4 text-gray-800">
         Galería de Superhéroes
       </h1>
 
       <div className="flex justify-center mb-8">
         <button
-          onClick={() => {
-            // TODO: Implementar función para recargar superhéroes
-          }}
+          // TODO: Implementar función para recargar superhéroes
+          onClick={(e) => {}}
           className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded transition-colors"
         >
           Recargar
